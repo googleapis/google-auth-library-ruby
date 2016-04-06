eb2570e6dbe0313e4eb416af86bcb687311b6a36
Omri Masri <omri@soom.la>
2016-04-06 18:46:15 +0300

    use hash as arguments for mongo methods, add documentation

--- lib/googleauth/stores/mongo_token_store.rb
+++ lib/googleauth/stores/mongo_token_store.rb
@@ -4,9 +4,18 @@ require 'googleauth/token_store'
 module Google
   module Auth
     module Stores
+      # Implementation of user token storage backed by Mongo. Tokens
+      # are stored in 'TokenStore' collection, in a document which look
+      # like this: { _id: ID, token: TOKEN}
       class MongoTokenStore < Google::Auth::TokenStore
         DEFAULT_COLLECTION = 'TokenStore'
 
+        # Create a new store with the supplied mongo client.
+        #
+        # @param [Mongo::Client] mongo
+        #  Initialized mongo client to connect to.
+        # @param [String] collection
+        #  Collection name for mongo. Defaults to 'TokenStore'
         def initialize(options = {})
 					collection = options.delete(:collection)
           @mongo = options.delete(:mongo)
@@ -15,18 +24,18 @@ module Google
 
         # (see Google::Auth::Stores::TokenStore#load)
         def load(id)
-          ans = @mongo[:TokenStore].find(:_id => id).to_a
-          ans.one? ? ans[0][:token] : nil
+          ans = @mongo[@collection].find({_id: id}, {limit: 1, projection: {_id: 0, token: 1}}).to_a
+          ans.any? ? ans[0][:token] : nil
         end
 
         # (see Google::Auth::Stores::TokenStore#store)
         def store(id, token)
-          @mongo[:TokenStore].update_one({_id: id}, {_id: id, token: token}, {upsert: true})
+          @mongo[@collection].update_one({_id: id}, {_id: id, token: token}, {upsert: true})
         end
 
         # (see Google::Auth::Stores::TokenStore#delete)
         def delete(id)
-          @mongo[:TokenStore].delete_one(:_id => id)
+          @mongo[@collection].delete_one({_id: id})
         end
       end
     end
