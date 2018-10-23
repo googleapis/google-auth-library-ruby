65ee0cbe3c6477d860455ef63ac18cb5aeb6386b
TheRoyalTnetennba <paye@google.com>
2018-10-23 16:56:00 -0700

    add exception for rubocop perceived complexity

--- lib/googleauth/credentials.rb
+++ lib/googleauth/credentials.rb
@@ -27,7 +27,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# rubocop:disable Metrics/AbcSize, MethodLength
+# rubocop:disable Metrics/AbcSize, Metrics/PerceivedComplexity, MethodLength
 
 require 'forwardable'
 require 'json'
