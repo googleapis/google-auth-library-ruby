b4feeb6188df86fd788eea073a60e6a9ed8726da
Graham Paye <gpaye8@gmail.com>
2019-04-04 13:20:58 -0700

    setup windows builds to track google-cloud-ruby (#209)

--- .kokoro/windows.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-script_url="https://raw.githubusercontent.com/googleapis/google-cloud-ruby/master/.kokoro/windows.sh"
-curl -o master-windows.sh $script_url && source master-windows.sh
