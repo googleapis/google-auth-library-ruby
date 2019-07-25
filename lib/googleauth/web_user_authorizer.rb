3c10231cad2ba7b4a20af76065189d4a0ca2cce2
Daniel Azuma <dazuma@google.com>
2019-07-25 14:08:48 -0700

    Fix arity of WebUserAuthorizer#get_credentials to be compatible with base class

--- lib/googleauth/web_user_authorizer.rb
+++ lib/googleauth/web_user_authorizer.rb
@@ -190,8 +190,8 @@ module Google
       # @raise [Signet::AuthorizationError]
       #  May raise an error if an authorization code is present in the session
       #  and exchange of the code fails
-      def get_credentials user_id, request, scope = nil
-        if request.session.key? CALLBACK_STATE_KEY
+      def get_credentials user_id, request = nil, scope = nil
+        if request && request.session.key?(CALLBACK_STATE_KEY)
           # Note - in theory, no need to check required scope as this is
           # expected to be called immediately after a return from authorization
           state_json = request.session.delete CALLBACK_STATE_KEY
