018ebce37d771c2cfe59094883c4866070f1e971
Daniel Azuma <dazuma@google.com>
2022-06-23 06:09:45 +0000

    feat: Update minimum Ruby version to 2.6

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,16 +20,6 @@ $LOAD_PATH.unshift spec_dir
 $LOAD_PATH.unshift lib_dir
 $LOAD_PATH.uniq!
 
-# set up coverage
-require "simplecov"
-require "coveralls"
-
-SimpleCov.formatters = [
-  Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter
-]
-SimpleCov.start
-
 require "faraday"
 require "rspec"
 require "logging"
