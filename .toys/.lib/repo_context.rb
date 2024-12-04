d366ca3cb02848c7fdbb51f9f5d7a6e81d830b2c
Daniel Azuma <dazuma@google.com>
2024-12-04 21:47:36 +0000

    tests: fix kokoro environment loading to get credentials from secret manager

--- .toys/.lib/repo_context.rb
+++ .toys/.lib/repo_context.rb
@@ -17,18 +17,18 @@ class RepoContext
 
   def self.load_kokoro_env
     return if @loaded_env
-
-    if ::ENV["KOKORO_GFILE_DIR"]
-      service_account = "#{::ENV['KOKORO_GFILE_DIR']}/service-account.json"
-      raise "#{service_account} is not a file" unless ::File.file? service_account
-      ::ENV["GOOGLE_APPLICATION_CREDENTIALS"] = service_account
-
-      filename = "#{::ENV['KOKORO_GFILE_DIR']}/ruby_env_vars.json"
-      raise "#{filename} is not a file" unless ::File.file? filename
-      env_vars = ::JSON.parse ::File.read filename
-      env_vars.each { |k, v| ::ENV[k] ||= v }
-    end
-
     @loaded_env = true
+
+    gfile_dir = ::ENV["KOKORO_GFILE_DIR"]
+    return unless gfile_dir
+
+    filename = "#{gfile_dir}/ruby_env_vars.json"
+    raise "#{filename} is not a file" unless ::File.file? filename
+    env_vars = ::JSON.parse ::File.read filename
+    env_vars.each { |k, v| ::ENV[k] ||= v }
+
+    filename = "#{gfile_dir}/secret_manager/ruby-main-ci-service-account"
+    raise "#{filename} is not a file" unless ::File.file? filename
+    ::ENV["GOOGLE_APPLICATION_CREDENTIALS"] = filename
   end
 end
