29065a5d98bc712541a76137ba9e2377e9d5fa8b
Viacheslav Rostovtsev <virost@google.com>
2025-04-12 08:09:39 -0700

    rubocooooop

--- samples/acceptance/authenticate_with_api_key_test.rb
+++ samples/acceptance/authenticate_with_api_key_test.rb
@@ -20,15 +20,13 @@ describe "authenticate_with_api_key" do
   let(:api_key) { ENV["GOOGLE_API_KEY"] }
   let(:stdout_output) { capture_io { authenticate_with_api_key api_key } }
 
-  skip "Requires real API key to run" do
-    it "authenticates with API key" do
-      # Skip the test if API key is not provided
-      skip "No API key available" if api_key.nil? || api_key.empty?
-      
-      output = stdout_output[0]
-      assert_includes output, "Text: Hello, world!"
-      assert_includes output, "Sentiment:"
-      assert_includes output, "Successfully authenticated using the API key"
-    end
+  it "authenticates with API key" do
+    # Skip the test if API key is not provided
+    skip "No API key available" if api_key.nil? || api_key.empty?
+
+    output = stdout_output[0]
+    assert_includes output, "Text: Hello, world!"
+    assert_includes output, "Sentiment:"
+    assert_includes output, "Successfully authenticated using the API key"
   end
 end
