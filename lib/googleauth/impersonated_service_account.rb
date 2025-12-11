90c064662c2252aa32c803e6197605e8e5fd58cf
Brad Sickles <brad.sickles@nullstone.io>
2025-12-11 11:29:41 -0500

    fix: missing auth header in impersonated service account credentials
    return correct auth header when preparing auth header

--- lib/googleauth/impersonated_service_account.rb
+++ lib/googleauth/impersonated_service_account.rb
@@ -296,9 +296,7 @@ module Google
       # @private
       # @return [Hash] The authorization header with the source credentials' token
       def prepare_auth_header
-        auth_header = {}
-        @source_credentials.updater_proc.call auth_header
-        auth_header
+        @source_credentials.updater_proc.call {}
       end
 
       # Makes the HTTP request to the impersonation endpoint.
